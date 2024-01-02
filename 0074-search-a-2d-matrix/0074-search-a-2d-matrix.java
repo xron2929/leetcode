b7cb57d2e07e34b00571466797118f2ec58d551a
xron2929 <xron2929@gmail.com>
2024-01-02 09:37:57 +0900

    Time: 7 ms (5.11%), Space: 42.6 MB (5.67%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,8 +1,23 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        boolean contains = Arrays.stream(matrix).flatMap(ints -> {
+       List<Integer> collect = Arrays.stream(matrix).flatMap(ints -> {
             return Arrays.stream(ints).boxed();
-        }).collect(Collectors.toList()).contains(target);
-        return contains;
+        }).collect(Collectors.toList());
+        int lt = 0;
+        int rt = collect.size() - 1;
+        while(lt<=rt) {
+            int mid= (lt+rt) / 2;
+            if(collect.get(mid)==target) {
+                return true;
+            }
+            else if(collect.get(mid)<target) {
+                lt = mid + 1;
+            }
+            else {
+                rt = mid - 1;
+            }
+        }
+        return false;
     }
+
 }
